7cbaddd3f9d22542fcca1df5868de28682fa6510
JPIGG <jon_piggott@yahoo.com>
2020-09-15 21:16:19 -0400

    Added basic structure for fileObject input and output for Account tracking.

--- Account.java
+++ Account.java
@@ -1,4 +1,4 @@
-
+import java.io.*;
 import java.util.*;
 
 public class Account {
@@ -29,14 +29,51 @@ public class Account {
 		}
 	}
 
-	public void loginAccount() {
-
+	public static void loginAccount(String username, String password) {
+		System.out.println("Login with username: " + username);
+		System.out.println("Login with password: " + password);
 	}
 
 	public static void main(String[] args) {
 
 		Scanner userInput = new Scanner(System.in);
 		int menuOption;
+
+		try {
+			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("Accounts.txt"));
+			for (Account accnt : accountList) {
+				if (accnt != null) {
+					outputStream.writeObject(accnt);
+				}
+			}
+			outputStream.close();
+		} catch (IOException e) {
+			System.out.println("Error in file writing");
+			e.printStackTrace();
+		}
+
+		try {
+			ObjectInputStream fileInput = new ObjectInputStream(new FileInputStream("Accounts.txt"));
+			while (true) {	
+				try {
+					Account loadAccount = (Account)fileInput.readObject();
+					accountList.add(loadAccount);
+				} catch (EOFException e) {
+					System.out.println("File has been loaded succesfully");
+					break;
+				}	
+			}		
+			fileInput.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Error in file writing");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error in file writing");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error in file writing");
+			e.printStackTrace();
+		}
 
 		do {
 			System.out.println("Enter: 0 = exit, 1 = new account, 2 = login");
@@ -63,7 +100,13 @@ public class Account {
 			} else {
 
 				System.out.println("Login for system...");
+				System.out.println("Enter your username: ");
+				String userNameInput = userInput.nextLine();
+				System.out.println("Enter your password: ");
+				String userPasswordInput = userInput.nextLine();
 
+				loginAccount(userNameInput, userPasswordInput);
+				printAccounts();
 			}
 		} while (menuOption != 0);
 
